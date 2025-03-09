1ce0c6ea6bf5f45f2815ca48f586275a30a562c4
Alexander <kondaurov.dev@gmail.com>
2025-03-09 15:50:23 +0400

    structured joins added

--- readme.md
+++ readme.md
@@ -1,12 +1,23 @@
 # Parser of Select Queries
 
+## What is This?
+
+This Kotlin-based parser is designed to parse only SELECT queries in SQL. Since the parser does not need to handle DELETE or UPDATE queries, I decided to focus exclusively on SELECT queries.
+
+I implemented a naive parsing algorithm—much like how a person might break down a query on paper. The process involves splitting the SQL string into groups of expressions (SELECT, FROM, WHERE, etc.) and then assembling these expressions to build a [Select Query](src/main/kotlin/core/SelectQuery.kt) class object.
+
 ## How to Run
-- Just run `Main.kt`.
-- You can also check and run the unit tests.
+
+Just run `Main.kt`.
+
+You can also check and run the unit tests.
 
 ## How It Works
-- A raw SQL string is exploded by [QueryStatements](src/main/kotlin/core/QueryStatements.kt).
-- It is then used to build a structured [Select Query](src/main/kotlin/core/SelectQuery.kt).
 
-## Notes on Parser Operation
-The WHERE clause can consist of complex conditions joined in various ways, so I decided to return the entire WHERE clause as a string.
+A raw SQL string is exploded by [QueryStatements](src/main/kotlin/core/QueryStatements.kt).
+
+These statements are then used to build a structured [Select Query](src/main/kotlin/core/SelectQuery.kt).
+
+## Notes
+
+- The WHERE/HAVING clause can consist of complex conditions joined in various ways, so I decided to return the entire WHERE/HAVING clause as a string.
--- src/main/kotlin/core/SelectQuery.kt
+++ src/main/kotlin/core/SelectQuery.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 data class SelectQuery(
     val columns: List<Column>,
     val from: List<Source>,
-    val joins: List<String>? = null,
+    val joins: List<Join>? = null,
     val where: String? = null,
     val groupBy: List<String>? = null,
     val orderBy: List<String>? = null,
@@ -23,7 +23,7 @@ data class SelectQuery(
             return SelectQuery(
                 columns = statements.getColumns().map { Column.fromString(it) },
                 from = statements.getFromSources().map { Source.fromString(it) },
-                joins = statements.getJoins(),
+                joins = statements.getJoins()?.map { Join.fromString(it) },
                 where = statements.getWhere(),
                 groupBy = statements.getGroupBy(),
                 orderBy = statements.getOrderBy(),
@@ -34,6 +34,37 @@ data class SelectQuery(
         }
 
 
+    }
+
+}
+
+@Serializable
+data class Join(
+    val type: JoinType,
+    val tableName: String,
+    val on: String
+) {
+
+    companion object {
+        fun fromString(input: String): Join {
+            val parts = input.split(" ON ")
+            if (parts.size != 2) throw Error("Invalid join structure '$input'")
+            val leftPart = parts[0].split(" ")
+            if (leftPart.size != 3) throw Error("Invalid join structure '$input'. Left part must consist of three words")
+            val joinType = JoinType.entries.find { it.name.equals(leftPart[0], true) }
+            if (joinType == null) throw Error("Invalid join structure '$input'. Invalid join type")
+            var on = parts[1]
+            if (on.startsWith("(") && on.endsWith(")")) on = on.substring(1, on.length - 1)
+            return Join(
+                type = joinType,
+                tableName = leftPart[2],
+                on
+            )
+        }
+    }
+
+    enum class JoinType {
+        inner, left, right, full
     }
 
 }
